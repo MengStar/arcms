f0440cbb535e09ce8e68a710bf0be2df507ac7b0
MengStar <641510128@qq.com>
2018-10-29 18:00:39 +0800

    新增环境项配置

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,9 +12,9 @@ apollo_portal_db_password=password
 
 # meta server url, different environments should have different meta server addresses
 dev_meta=http://localhost:8080
-#fat_meta=http://fill-in-fat-meta-server:8080
-#uat_meta=http://fill-in-uat-meta-server:8080
-#pro_meta=http://fill-in-pro-meta-server:8080
+fat_meta=http://localhost:8080
+uat_meta=http://localhost:8080
+pro_meta=http://localhost:8080
 
 META_SERVERS_OPTS="-Ddev_meta=$dev_meta -Dfat_meta=$fat_meta -Duat_meta=$uat_meta -Dpro_meta=$pro_meta"
 
