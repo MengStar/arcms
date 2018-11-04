69d1c209f83b9d2be2120dfe179d6f0617b9ca0c
MengStar <641510128@qq.com>
2018-11-04 16:49:07 +0800

    configservice format

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/metaservice/controller/ServiceController.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/metaservice/controller/ServiceController.java
@@ -3,14 +3,12 @@ package com.ctrip.framework.apollo.metaservice.controller;
 import com.ctrip.framework.apollo.core.dto.ServiceDTO;
 import com.ctrip.framework.apollo.metaservice.service.DiscoveryService;
 import com.netflix.appinfo.InstanceInfo;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,19 +25,7 @@ public class ServiceController {
     @RequestMapping("/meta")
     public List<ServiceDTO> getMetaService() {
         List<InstanceInfo> instances = discoveryService.getMetaServiceInstances();
-        List<ServiceDTO> result = instances.stream().map(new Function<InstanceInfo, ServiceDTO>() {
-
-            @Override
-            public ServiceDTO apply(InstanceInfo instance) {
-                ServiceDTO service = new ServiceDTO();
-                service.setAppName(instance.getAppName());
-                service.setInstanceId(instance.getInstanceId());
-                service.setHomepageUrl(instance.getHomePageUrl());
-                return service;
-            }
-
-        }).collect(Collectors.toList());
-        return result;
+        return getServiceDTOS(instances);
     }
 
     /**
@@ -53,17 +39,16 @@ public class ServiceController {
             @RequestParam(value = "appId", defaultValue = "") String appId,
             @RequestParam(value = "ip", required = false) String clientIp) {
         List<InstanceInfo> instances = discoveryService.getConfigServiceInstances();
-        List<ServiceDTO> result = instances.stream().map(new Function<InstanceInfo, ServiceDTO>() {
+        return getServiceDTOS(instances);
+    }
 
-            @Override
-            public ServiceDTO apply(InstanceInfo instance) {
-                ServiceDTO service = new ServiceDTO();
-                service.setAppName(instance.getAppName());
-                service.setInstanceId(instance.getInstanceId());
-                service.setHomepageUrl(instance.getHomePageUrl());
-                return service;
-            }
-
+    private List<ServiceDTO> getServiceDTOS(List<InstanceInfo> instances) {
+        List<ServiceDTO> result = instances.stream().map(instance -> {
+            ServiceDTO service = new ServiceDTO();
+            service.setAppName(instance.getAppName());
+            service.setInstanceId(instance.getInstanceId());
+            service.setHomepageUrl(instance.getHomePageUrl());
+            return service;
         }).collect(Collectors.toList());
         return result;
     }
@@ -75,18 +60,6 @@ public class ServiceController {
     @RequestMapping("/admin")
     public List<ServiceDTO> getAdminService() {
         List<InstanceInfo> instances = discoveryService.getAdminServiceInstances();
-        List<ServiceDTO> result = instances.stream().map(new Function<InstanceInfo, ServiceDTO>() {
-
-            @Override
-            public ServiceDTO apply(InstanceInfo instance) {
-                ServiceDTO service = new ServiceDTO();
-                service.setAppName(instance.getAppName());
-                service.setInstanceId(instance.getInstanceId());
-                service.setHomepageUrl(instance.getHomePageUrl());
-                return service;
-            }
-
-        }).collect(Collectors.toList());
-        return result;
+        return getServiceDTOS(instances);
     }
 }
