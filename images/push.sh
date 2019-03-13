e4fbaacf73126c152f43a304a072643524359ce7
MengStar <641510128@qq.com>
2019-03-13 14:09:02 +0800

    push 脚本

---
+++ images/push.sh
@@ -0,0 +1,7 @@
+docker tag portal registry.cn-hangzhou.aliyuncs.com/mengstars/portal
+docker tag sync registry.cn-hangzhou.aliyuncs.com/mengstars/sync
+docker tag admin registry.cn-hangzhou.aliyuncs.com/mengstars/admin
+docker push registry.cn-hangzhou.aliyuncs.com/mengstars/portal
+docker push registry.cn-hangzhou.aliyuncs.com/mengstars/sync
+docker push registry.cn-hangzhou.aliyuncs.com/mengstars/admin
+
